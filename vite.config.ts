828e4d4e37d7eb04e4fd78db619dfd504f42e24a
asadahimeka <smallstar9637@yahoo.co.jp>
2022-12-05 03:19:10 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,9 @@ export default defineConfig({
         clientsClaim: true,
         navigateFallbackDenylist: [
           /^\/api/,
+          /^\/-/,
+          /^\/~/,
+          /^\/(ajax|rpc|.+\.php)/,
         ],
         runtimeCaching: [
           {
