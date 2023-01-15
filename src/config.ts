9a79c5c42b179c93d1fdd99e4bcc3e587ab35c3f
asadahimeka <smallstar9637@yahoo.co.jp>
2023-01-15 22:47:45 +0800

    f

--- src/config.ts
+++ src/config.ts
@@ -29,6 +29,9 @@ export const IMAGE_CACHE_SECONDS = 12 * 60 * 60 * 1000
 
 export function resolveSrc(src?: string | null) {
   if (!src) return ''
+  if (src.includes('s.pximg.net')) {
+    return src
+  }
   if (src.startsWith('/~/')) {
     return 'https://s.pximg.net' + src.replace('/~/', '/')
   }
